05961cbac54ffb684047beb932eaac91ad782921
Erik Rumppe <erumppe@gmail.com>
2015-01-06 15:27:17 -0800

    Updates to templates

--- src/templates/testing/Controller.groovy
+++ src/templates/testing/Controller.groovy
@@ -1,9 +1,6 @@
 @artifact.package@import grails.test.mixin.TestFor
 import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
- */
 @TestFor(@artifact.testclass@)
 class @artifact.name@ extends Specification {
 
--- src/templates/testing/DomainClass.groovy
+++ src/templates/testing/DomainClass.groovy
@@ -1,9 +1,6 @@
 @artifact.package@import grails.test.mixin.TestFor
 import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
- */
 @TestFor(@artifact.testclass@)
 class @artifact.name@ extends Specification {
 
