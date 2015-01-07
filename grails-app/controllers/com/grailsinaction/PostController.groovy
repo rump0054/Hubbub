3dfd5f7432ab3ef947a773f5feb056e8b5c49b1d
Erik Rumppe <erumppe@gmail.com>
2015-01-07 15:16:51 -0800

    Doing a standard commit

--- grails-app/controllers/com/grailsinaction/PostController.groovy
+++ grails-app/controllers/com/grailsinaction/PostController.groovy
@@ -1,5 +1,29 @@
 package com.grailsinaction
 
 class PostController {
-    static scaffold = true
+  static scaffold = true
+
+  def timeline() {
+    def user = User.findByLoginId(params.id)
+    if (!user) {
+      resonse.sendError(404)
+    } else {
+      [ user: user ]
+    }
+  }
+
+  def addPost() {
+    def user = User.findByLoginId(params.id)
+    if (user) {
+      def post = new Post(params)
+      user.addToPosts(post)
+      if (user.save()) {
+        flash.message = "Successfully created post"
+      } else {
+        flash.message = "Invalid or empty post"
+      }
+    }
+
+    redirect(action: 'timeline', id: params.id)
+  }    
 }
