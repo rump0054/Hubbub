6f4432217af4c0e6f11d87c42a9eb8568873290a
Erik Rumppe <erumppe@gmail.com>
2015-01-08 17:10:35 -0800

    Changed default package

--- test/unit/com/grailsinaction/PostControllerSpec.groovy
+++ test/unit/com/grailsinaction/PostControllerSpec.groovy
@@ -3,102 +3,123 @@ package com.grailsinaction
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
-/**
- * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
- */
 @TestFor(PostController)
-@Mock([User,Post])
+@Mock([User, Post, LameSecurityFilters])
 class PostControllerSpec extends Specification {
 
-  def "Get a users timeline given their id"() {
-      given: "A user with posts in the db"
-      User chuck = new User(
-              loginId: "chuck_norris",
-              password: "password")
-      chuck.addToPosts(new Post(content: "A first post"))
-      chuck.addToPosts(new Post(content: "A second post"))
-      chuck.save(failOnError: true)
+    def "Get a users timeline given their id"() {
+        given: "A user with posts in the db"
+        User chuck = new User(
+                loginId: "chuck_norris",
+                password: "password")
+        chuck.addToPosts(new Post(content: "A first post"))
+        chuck.addToPosts(new Post(content: "A second post"))
+        chuck.save(failOnError: true)
 
-      and: "A loginId parameter"
-      params.id = chuck.loginId
+        and: "A loginId parameter"
+        params.id = chuck.loginId
 
-      when: "the timeline is invoked"
-      def model = controller.timeline()
+        when: "the timeline is invoked"
+        def model = controller.timeline()
 
-      then: "the user is in the returned model"
-      model.user.loginId == "chuck_norris"
-      model.user.posts.size() == 2
-  }
+        then: "the user is in the returned model"
+        model.user.loginId == "chuck_norris"
+        model.user.posts.size() == 2
+    }
 
-  def "Check that non-existent users are handled with an error"() {
+    def "Check that non-existent users are handled with an error"() {
 
-      given: "the id of a non-existent user"
-      params.id = "this-user-id-does-not-exist"
+        given: "the id of a non-existent user"
+        params.id = "this-user-id-does-not-exist"
 
-      when: "the timeline is invoked"
-      def model = controller.timeline()
+        when: "the timeline is invoked"
+        controller.timeline()
 
-      then: "a 404 is sent to the browser"
-      response.status == 404
+        then: "a 404 is sent to the browser"
+        response.status == 404
 
-  }
+    }
 
-  def "Adding a valid new post to the timeline"() {
-      given: "A user with posts in the db"
-      User chuck = new User(
-              loginId: "chuck_norris",
-              password: "password").save(failOnError: true)
+    def "Adding a valid new post to the timeline"() {
+        given: "a mock post service"
+        def mockPostService = Mock(PostService)
+        1 * mockPostService.createPost(_, _) >> new Post(content: "Mock Post")
+        controller.postService = mockPostService
 
-      and: "A loginId parameter"
-      params.id = chuck.loginId
+        when:  "controller is invoked"
+        def result = controller.addPost(
+                "joe_cool",
+                "Posting up a storm")
 
-      and: "Some content for the post"
-      params.content = "Chuck Norris can unit test entire applications with a single assert."
+        then: "redirected to timeline, flash message tells us all is well"
+        flash.message ==~ /Added new post: Mock.*/
+        response.redirectedUrl == '/users/joe_cool'   
 
-      when: "addPost is invoked"
-      def model = controller.addPost()
+        // Without the custom URL mapping, the check would be this:
+//        response.redirectedUrl == '/post/timeline/joe_cool'   
 
-      then: "our flash message and redirect confirms the success"
-      flash.message == "Successfully created post"
-      response.redirectedUrl == "/post/timeline/${chuck.loginId}"
-      Post.countByUser(chuck) == 1
+    }
 
-  }
+    def "Adding an invalid new post to the timeline"() {
+        given: "A user with posts in the db"
+        User chuck = new User(loginId: "chuck_norris", password: "password").save(failOnError: true)
 
-  def "Adding an invalid new post to the timeline"() {
-      given: "A user with posts in the db"
-      User chuck = new User(loginId: "chuck_norris", password: "password").save(failOnError: true)
+        and: "A post service that throws an exception with the given data"
+        def errorMsg = "Invalid or empty post"
+        def mockPostService = Mock(PostService)
+        controller.postService = mockPostService
+        1 * mockPostService.createPost(chuck.loginId, null) >> { throw new PostException(message: errorMsg) }
 
-      and: "A loginId parameter"
-      params.id = chuck.loginId
+        and: "A loginId parameter"
+        params.id = chuck.loginId
 
-      and: "Some content for the post"
-      params.content = null
+        and: "Some content for the post"
+        params.content = null
 
-      when: "addPost is invoked"
-      def model = controller.addPost()
+        when: "addPost is invoked"
+        def model = controller.addPost()
 
-      then: "our flash message and redirect confirms the success"
-      flash.message == "Invalid or empty post"
-      response.redirectedUrl == "/post/timeline/${chuck.loginId}"
-      Post.countByUser(chuck) == 0
-  }
+        then: "our flash message and redirect confirms the success"
+        flash.message == errorMsg
+        response.redirectedUrl == "/users/${chuck.loginId}"
+        Post.countByUser(chuck) == 0
 
-  @spock.lang.Unroll
-  def "Testing id of #suppliedId redirects to #expectedURL"() {
-    given:
-    params.id = suppliedId
+        // Without the custom URL mapping, the check would be this:
+//        response.redirectedUrl == "/post/timeline/${chuck.loginId}"
+    }
 
-    when: "Controller is invoked"
-    controller.home()
+    @Unroll                                             
+    def "Testing id of #suppliedId redirects to #expectedUrl"() {  
+                                                                   
+        given:                                                     
+        params.id = suppliedId                                     
+                                                                   
+        when: "Controller is invoked"                              
+        controller.home()                                         
+                                                                   
+        then:                                                      
+        response.redirectedUrl == expectedUrl                      
+                                                                   
+        where:                                                     
+        suppliedId  |   expectedUrl                                
+        'joe_cool'  |   '/users/joe_cool'                  
+        null        |   '/users/chuck_norris'              
+                                                                   
+    }
+    
+    def "Exercising security filter for unauthenticated user"() {
 
-    then:
-    response.redirectedUrl == expectedURL
+        when:
+        withFilters(action: "addPost") {
+            controller.addPost("glen_a_smith", "A first post")
+        }
 
-    where:
-    suppliedId  | expectedURL
-    'joe_cool'  | '/post/timeline/joe_cool'
-    null        | '/post/timeline/chuck_norris'
-  }
+        then:
+        response.redirectedUrl == '/login/form'
+
+    }
+
 }
+
