6d5abbb616f6bcfb0b539c59c46455ca9bb8ce4a
Erik Rumppe <erumppe@gmail.com>
2015-04-07 10:43:31 -0500

    Finished with events listener stuff.  Doesnt work as described in the book.  Shocking.... not really.

--- grails-app/services/com/grailsinaction/AuditService.groovy
+++ grails-app/services/com/grailsinaction/AuditService.groovy
@@ -1,10 +1,34 @@
 package com.grailsinaction
 
-class AuditService {
-  static transactional = false
+//import org.springframework.security.authentication.event.AuthenticationSuccessEvent
 
-  @grails.events.Listener
-	def onNewPost(Post newPost) {
-    log.error "New Post from: ${newPost.user.loginId} : ${newPost.shortContent}"
+class AuditService {
+
+	/*static transactional = false
+
+	def springSecurityService
+
+	@grails.events.Listener
+	def onNewPost(Post newPost){
+		log.error "New Post from: ${newPost.user.loginId} : ${newPost.shortContent}"
 	}
+
+	@grails.events.Listener(namespace = 'gorm')
+	void onSaveOrUpdate(User user) {
+		if (springSecurityService.isLoggedIn()) {
+			log.error "Changes made to account ${user?.loginId} by ${springSecurityService.currentUser}"
+		}
+	}
+
+	@grails.events.Listener(namespace = 'gorm')
+	void onSaveOrUpdate(Post post) {
+		if (springSecurityService.isLoggedIn()) {
+			log.error "New Post Created: ${post?.content} by ${springSecurityService.currentUser}"
+		}
+	}
+
+	@grails.events.Listener(namespace = "security")
+	def onUserLogin(AuthenticationSuccessEvent loginEvent){
+		log.error "We appeared to have logged in a user: ${loginEvent.authentication.principal.username}"
+	}*/
 }
